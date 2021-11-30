df0fedb9f5afe5b20a3fa16b66aec1fe3bc54799
BleezeAndBlueSky <yr.963@163.com>
2021-11-30 00:16:37 -0500

    use MATLAB Coder to impliment Mq & Cqdq_dq

---
+++ include/matlab/Cqdq_dq.h
@@ -0,0 +1,26 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// Cqdq_dq.h
+//
+// Code generation for function 'Cqdq_dq'
+//
+
+#ifndef CQDQ_DQ_H
+#define CQDQ_DQ_H
+
+// Include files
+#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void Cqdq_dq(double dth1, double dth2, double dth3, double dth4,
+                    double dth5, double dth6, double th1, double th2,
+                    double th3, double th4, double th5, double th6,
+                    double b_Cqdq_dq[6]);
+
+#endif
+// End of code generation (Cqdq_dq.h)
---
+++ include/matlab/Mq.h
@@ -0,0 +1,24 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// Mq.h
+//
+// Code generation for function 'Mq'
+//
+
+#ifndef MQ_H
+#define MQ_H
+
+// Include files
+#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void Mq(double th1, double th2, double th3, double th4, double th5,
+               double th6, double b_Mq[36]);
+
+#endif
+// End of code generation (Mq.h)
